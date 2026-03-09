e924d12939c8ed39913c38ed380583939eb390ff
MD Akram Hossan Bappy <akram.hossan@portonics.com>
2026-03-09 14:46:03 +0000

    day 02

---
+++ qurey_param.py
@@ -0,0 +1,38 @@
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+# fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+# @app.get("/items/")
+# async def read_item(skip: int = 0, limit: int = 10):
+#     return fake_items_db[skip : skip + limit]
+
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: str, q: str | None = None):
+#     if q:
+#         return {"item_id": item_id, "q": q}
+#     return {"item_id": item_id}
+
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
